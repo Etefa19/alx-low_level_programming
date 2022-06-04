9dfcaa19f5010ea3bd30bce191bba5fde1e00b07
Etefa19 <yenuangasu134@gmail.com>
2022-06-04 13:39:31 -0700

    second script

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- *main - prints the alphabet in lowercase,
+ *main - prints all single digit numbers of base 10 starting from 0,
  *followed by a new line
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch;
+	int n;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (n = 48; n < 58; n++)
 	{
-		putchar(ch);
+		putchar(n);
 	}
 	putchar('\n');
 	return (0);
 }
+
