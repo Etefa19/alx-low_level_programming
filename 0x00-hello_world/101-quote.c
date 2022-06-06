cada45d7c17494f3ebafa5cf00538c318115e29c
Etefa19 <yenuangasu134@gmail.com>
2022-06-06 10:44:36 -0700

    second script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints using the function 
+ * main - prints using the function; 
  *
- * return: Always 0 (success)
+ * return: Always 0 (success);
  * */
 int main(void)
 {
 char err[] = "and the pieceof art id useful" - Dora korpar, 2015-10-19\n;
-write(2,err,59)
+write(2,err,59);
 return (1);
 }
