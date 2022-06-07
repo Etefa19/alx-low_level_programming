7ca4f7b1c722af1fc5421f92dbcc8af8261b8b0b
Etefa19 <yenuangasu134@gmail.com>
2022-06-06 18:10:32 -0700

    first script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints using the function; 
- *
- * return: Always 0 (success);
- * */
+* main - prints using the function
+*
+*Return: Always 0 (success)
+*/
 int main(void)
 {
-char err[] = "and the pieceof art id useful" - Dora korpar, 2015-10-19\n;
-write(2,err,59);
+char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, err, 59);
 return (1);
 }
